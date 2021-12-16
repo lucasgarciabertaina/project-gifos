f0b8137bf48a4e2890bf31a295c87f89601dc491
Lucas García Bertaina <lucasgarciabertaina@gmail.com>
2021-12-15 21:09:48 -0300

    work into search

--- apps/modules/page.js
+++ apps/modules/page.js
@@ -1,11 +1,12 @@
 import darkMode from './darkMode.js';
 import setTrendingGifos from './trending/setTrendingGifos.js';
-
+import search from "../modules/search/search.js"
 export function page(htmlPage) {
   switch (htmlPage) {
     case index:
       darkMode('index');
       setTrendingGifos();
+      search();
       //eventListeners de search
       break;
 
