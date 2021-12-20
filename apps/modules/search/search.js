ea5ec1304dccae7c1da67eb55327e5e85db52504
Lucas García Bertaina <lucasgarciabertaina@gmail.com>
2021-12-20 12:57:31 -0300

    Darkmode donde, search in progress

--- apps/modules/search/search.js
+++ apps/modules/search/search.js
@@ -4,7 +4,7 @@ const API_KEY = "DENft9K6PwvYPg4VZEJmkeY2iciLh2yb";
 export default () => {
   const searcher = document.getElementById("searcher");
   searcher.addEventListener("keydown", async (letter) => {
-    console.log(letter);
+    console.log(searcher.value += letter);
     console.log(searcher.value);
     if (searcher.value.length > 2) {
       console.log(await search(API_KEY, searcher.value))
