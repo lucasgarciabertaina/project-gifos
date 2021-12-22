125c6ab6f97954ddca8f1059dfd2549cd241b21b
Lucas García Bertaina <lucasgarciabertaina@gmail.com>
2021-12-22 12:24:35 -0300

    Style of suggestions complete, the application with the api is missing

--- apps/modules/search/setOptions.js
+++ apps/modules/search/setOptions.js
@@ -1,3 +1,34 @@
 export default (gifoOptions) => {
-  console.log(gifoOptions);
+  const searcherContainer = document.getElementById("searcher-container")
+    .removeAttribute("class")
+    .setAttribute("class", "search-section__search--active");
+  document.getElementById("search")
+    .removeAttribute("class")
+    .setAttribute("class", "search--active");
+  document.getElementById("divisor")
+    .removeAttribute("class")
+    .setAttribute("search-section__divisor--active");
+  document.getElementById("icon-search")
+    .removeAttribute("class")
+    .setAttribute("search-section__icon-search--active");
+  document.getElementById("icon-close")
+    .removeAttribute("class")
+    .setAttribute("class", "search-section__icon-close--active");
+  // const searcher = document.getElementById("searcher");
+
+  for (const suggestion of gifoOptions) {
+    const iconSearch = document.createElement("span").setAttribute("class", "option__icon-search")
+    const gifoSuggestion = document.createElement("h4").setAttribute("class", "option__sugerence");
+    const option = document.createElement("div")
+      .setAttribute("class", "option")
+      .appendChild(iconSearch)
+      .appendChild(gifoSuggestion)
+    option.innerHTML = suggestion;
+    searcherContainer.appendChild(option);
+  }
 }
+
+{/* <div class="option">
+            <span class="option__icon-search"></span>
+            <h4 class="option__sugerence">Sugerencia 1</h4>
+          </div> */}
