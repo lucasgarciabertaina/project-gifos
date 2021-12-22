f23e4d49cc30283c471f288c7ef3f0b7a5694ac4
Lucas García Bertaina <lucasgarciabertaina@gmail.com>
2021-12-21 21:57:30 -0300

    Style of searcher in progress

--- apps/modules/search/search.js
+++ apps/modules/search/search.js
@@ -1,13 +1,17 @@
 import search from './get.js';
+import setOptions from './setOptions.js';
 const API_KEY = "DENft9K6PwvYPg4VZEJmkeY2iciLh2yb";
 
-export default () => {
+
+export default async () => {
   const searcher = document.getElementById("searcher");
+
   searcher.addEventListener("keydown", async (letter) => {
-    console.log(searcher.value += letter);
-    console.log(searcher.value);
-    if (searcher.value.length > 2) {
-      console.log(await search(API_KEY, searcher.value))
+    const value = searcher.value + letter.key;
+    if (value.length > 1) {
+      const searchValues = await search(API_KEY, value);
+      const gifosTitles = searchValues.map(item => item.title);
+      setOptions(gifosTitles);
     }
   })
 }
---
+++ apps/modules/search/setOptions.js
@@ -0,0 +1,3 @@
+export default (gifoOptions) => {
+  console.log(gifoOptions);
+}
